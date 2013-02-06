aa2e320f1fe8a395b9ac8d669a7d403f7ea27081
dholtzap <dholtzap@kec119-07.ycp.edu>
2013-02-06 09:46:46 -0500

    added another print statement "hi"

--- Calculator/src/edu/ycp/cs320/calculator/Main.java
+++ Calculator/src/edu/ycp/cs320/calculator/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the calculator!");
 		System.out.println("Yeah, this is a great program");
+		System.out.println("Hi");
 	}
 }
